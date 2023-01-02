c16cce27f06da8b13bc4c419c002c426f6c8cdb3
KaijiKoami <co.network.biz@gmail.com>
2023-01-02 14:25:08 +0900

    pd

---
+++ index.js
@@ -0,0 +1,48 @@
+import liff from '@line/liff'
+import './style.css';
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    liff.init({ 
+            liffId: '1657773262-O5ENDkKN'
+        })
+        .then(() => {
+            initializeApp();
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    // liff
+    //     .getProfile()
+    //     .then((profile) => {
+    //         const name = profile.displayName;
+    //     })
+    //     .catch((err) => {
+    //         console.log("error", err);
+    //     });
+});
+
+function initializeApp() {
+    // ログインチェック
+    if (liff.isLoggedIn()) {
+        //ログイン済
+        getLineData();
+    } else {
+        // 未ログイン
+        let result = window.confirm("LINE Loginしますか？");
+        if(result) {
+            liff.login();
+        }
+    }
+}
+
+function getLineData() {
+    liff.getProfile()
+    .then(profile => {
+      const userId = profile.userId;
+    //   const displayName = profile.displayName;
+
+      document.getElementById('useridfield').value = userId;
+    })
+}
