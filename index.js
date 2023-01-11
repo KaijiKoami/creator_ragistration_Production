aa5c5a9f2eb92331981c33bd302528c203774894
KaijiKoami <co.network.biz@gmail.com>
2023-01-11 12:31:21 +0900

    test

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ function getLineData() {
     liff.getProfile()
     .then(profile => {
 		const userId = profile.userId;
-		const displayName = profile.displayName;
+	//	const displayName = profile.displayName;
     //   const displayName = profile.displayName;
 
 		document.getElementById('useridfield').value = userId;
-		document.getElementById('displayNamefield').value = displayName;
+	//	document.getElementById('displayNamefield').value = displayName;
     })
 }
